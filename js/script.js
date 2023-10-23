72e29b3f16f6f0dca691244d53965ebea99e32d6
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2023-10-23 16:53:38 +0200

    add: ex 1 js script (v1)

--- js/script.js
+++ js/script.js
@@ -1,20 +1,15 @@
-/*let studentNumber = parseInt(prompt("Inserire numero studenti"));
-let students = [studentNumber];*/
+const mailList = ["mickey@mouse.com", "donald@duck.com", "goofy@goof.com"]
+let userMail = "";
 
-// let userNumber = "";
-// const students = ["Paolo", "Carlo", "Franco", "Gianni"]
-// let studentName = "";
-
-// for(i = 0; i < 1;) {
-//     userNumber = parseInt(prompt("Dammi un numero da 0 a 3"));
-//     if (userNumber < 0 || userNumber > 3){
-//         alert("Ho detto tra 0 e 3");
-//     } else {
-//         i++;
-//         document.getElementById("result").innerHTML = students[userNumber];
-//         console.log(students[userNumber]);
-//     }
-// }
+for(i = 0; i < 1; i++) {
+    userMail = prompt("Inserire mail");
+    if (userMail === mailList[i]){
+        alert("Welcome!");
+    } else {
+        alert("Access Denied!");
+        console.warn("Access Denied!");
+    }
+}
 
 
 
