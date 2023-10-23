53a113d1536d1a77513044b645a7da5d6fcae036
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2023-10-23 16:14:09 +0200

    scaffolding

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/*let studentNumber = parseInt(prompt("Inserire numero studenti"));
+let students = [studentNumber];*/
+
+// let userNumber = "";
+// const students = ["Paolo", "Carlo", "Franco", "Gianni"]
+// let studentName = "";
+
+// for(i = 0; i < 1;) {
+//     userNumber = parseInt(prompt("Dammi un numero da 0 a 3"));
+//     if (userNumber < 0 || userNumber > 3){
+//         alert("Ho detto tra 0 e 3");
+//     } else {
+//         i++;
+//         document.getElementById("result").innerHTML = students[userNumber];
+//         console.log(students[userNumber]);
+//     }
+// }
+
+
+
+
