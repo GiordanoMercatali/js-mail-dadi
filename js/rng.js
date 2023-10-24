a914b7e40a80b132ce4b224fb9caf138ac6a0118
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2023-10-24 10:58:43 +0200

    add: ex 2 winner becomes green, loser becomes red

--- js/rng.js
+++ js/rng.js
@@ -10,16 +10,37 @@ console.log(userNumber);
 let pcNumber = getRandomNumber(1,6);
 console.log(pcNumber); */
 
+userNumberElem = document.querySelector(".user-number");
+pcNumberElem = document.querySelector(".pc-number");
+
 const userNumber = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
 console.log(userNumber);
+document.getElementById("user-n").innerHTML = userNumber;
 
 const pcNumber = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
 console.log(pcNumber);
+document.getElementById("pc-n").innerHTML = pcNumber;
+
 
 if (userNumber > pcNumber){
     console.log("You win!");
+    document.getElementById("result").innerHTML = "You win!";
+    userNumberElem.classList.add("winner");
+    userNumberElem.classList.remove("loser");
+    pcNumberElem.classList.add("loser");
+    pcNumberElem.classList.remove("winner");
 } else if (userNumber < pcNumber){
     console.log("You lose!");
+    document.getElementById("result").innerHTML = "You lose!";
+    userNumberElem.classList.add("loser");
+    userNumberElem.classList.remove("winner");
+    pcNumberElem.classList.add("winner");
+    pcNumberElem.classList.remove("loser");
 } else{
     console.log("Draw");
+    document.getElementById("result").innerHTML = "Draw";
+    userNumberElem.classList.remove("loser");
+    userNumberElem.classList.remove("winner");
+    pcNumberElem.classList.remove("winner");
+    pcNumberElem.classList.remove("loser");
 }
